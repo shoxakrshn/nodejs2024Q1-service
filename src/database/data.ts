d8ed212e8d3f627e37b0ec568376f849edb92772
shoxa <sllx.ok@gmail.com>
2024-03-06 02:16:52 +0500

    refactor: remove database.ts

--- src/database/data.ts
+++ src/database/data.ts
@@ -1,5 +1,6 @@
 import { Album } from 'src/album/album.model';
 import { Artist } from 'src/artist/artist.model';
+import { Favs } from 'src/favs/favs.model';
 import { Track } from 'src/track/track.model';
 import { User } from 'src/user/user.model';
 
@@ -105,3 +106,21 @@ export const tracks: Track[] = [
     duration: 264,
   },
 ];
+
+export const userDb = new Map<string, User>(
+  users.map((user) => [user.id, user]),
+);
+
+export const artistDb = new Map<string, Artist>(
+  artists.map((artist) => [artist.id, artist]),
+);
+
+export const albumDb = new Map<string, Album>(
+  albums.map((album) => [album.id, album]),
+);
+
+export const trackDb = new Map<string, Track>(
+  tracks.map((track) => [track.id, track]),
+);
+
+export const favoriteDb: Favs = new Favs();
--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -4,6 +4,7 @@ import { Artist } from 'src/artist/artist.model';
 import { Favs } from 'src/favs/favs.model';
 import { Track } from 'src/track/track.model';
 import { User } from 'src/user/user.model';
+// import { userDb, artistDb, albumDb, trackDb, favoriteDb } from './data';
 
 @Injectable()
 export class DatabaseService {
@@ -12,4 +13,10 @@ export class DatabaseService {
   public readonly albums = new Map<string, Album>();
   public readonly tracks = new Map<string, Track>();
   public readonly favs = new Favs();
+
+  // public readonly users = userDb;
+  // public readonly artists = artistDb;
+  // public readonly albums = albumDb;
+  // public readonly tracks = trackDb;
+  // public readonly favs = favoriteDb;
 }
