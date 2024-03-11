9c08e725598c7237434f8ddae64a8429ffa731e9
shoxa <sllx.ok@gmail.com>
2024-03-12 01:35:51 +0500

    refactor: remove useless code

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -4,7 +4,6 @@ import { Artist } from 'src/artist/entities/artist.entity';
 import { Favs } from 'src/favs/entities/favs.entity';
 import { Track } from 'src/track/entities/track.entity';
 import { User } from 'src/user/entities/user.entity';
-// import { userDb, artistDb, albumDb, trackDb, favoriteDb } from './data';
 
 @Injectable()
 export class DatabaseService {
@@ -13,10 +12,4 @@ export class DatabaseService {
   public readonly albums = new Map<string, Album>();
   public readonly tracks = new Map<string, Track>();
   public readonly favs = new Favs();
-
-  // public readonly users = userDb;
-  // public readonly artists = artistDb;
-  // public readonly albums = albumDb;
-  // public readonly tracks = trackDb;
-  // public readonly favs = favoriteDb;
 }
